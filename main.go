1a2b709994228b3fcb1d0c997ee110ddbf9f78b4
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2024-02-21 17:02:01 +0100

    Bug fix

--- main.go
+++ main.go
@@ -49,10 +49,13 @@ func redirect(w http.ResponseWriter, r *http.Request) {
 func watch(path string, cfgptr *config) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("watch", "fsnotify.NewWatcher", err)
 	}
 	defer watcher.Close()
-	watcher.Add(path)
+	if err := watcher.Add(filepath.Dir(path)); err != nil {
+		log.Println("watch", "watcher.Add", err)
+		return
+	}
 
 loop:
 	for {
@@ -61,46 +64,37 @@ loop:
 			if !ok {
 				continue loop
 			}
-			if event.Has(fsnotify.Write) {
-				newcfg, err := loadConfig(path)
-				if err != nil {
-					log.Printf("error: failed to load config: %v", err)
-					break
+			if event.Has(fsnotify.Write) && event.Name == path {
+				if _, err := toml.DecodeFile(path, cfgptr); err != nil {
+					log.Println("watch", "toml.DecodeFile", err)
 				}
-				*cfgptr = newcfg
 			}
 
 		case err, ok := <-watcher.Errors:
 			if !ok {
 				break
 			}
-			log.Println(err)
+			log.Println("watch", err)
 		}
 	}
 }
 
-func loadConfig(path string) (cfg config, err error) {
-	_, err = toml.DecodeFile(path, &cfg)
-	return
-}
-
-func getConfig(path string) (cfg config) {
+func getConfig(path string) (c config) {
 	flag.StringVar(&path, "cfg", path, "Path to the configuration file")
-	flag.StringVar(&cfg.Port, "port", ":80", "The port that onering will listen to")
-	flag.StringVar(&cfg.TLSPort, "tlsport", ":443", "The TLS port that onering will listen to")
-	flag.StringVar(&cfg.Cert, "cert", "", "Path to the TLS certificate")
-	flag.StringVar(&cfg.Key, "key", "", "Path to the TLS key")
+	flag.StringVar(&c.Port, "port", ":80", "The port that onering will listen to")
+	flag.StringVar(&c.TLSPort, "tlsport", ":443", "The TLS port that onering will listen to")
+	flag.StringVar(&c.Cert, "cert", "", "Path to the TLS certificate")
+	flag.StringVar(&c.Key, "key", "", "Path to the TLS key")
 	flag.Parse()
 
-	cfg, err := loadConfig(path)
+	_, err := toml.DecodeFile(path, &c)
 	if err != nil {
-		log.Println(err)
+		log.Println("getConfig", "toml.DecodeFile", err)
 	}
-
-	if cfg.isZero() {
+	if c.isZero() {
 		log.Fatal("error: no configuration provided")
 	}
-	return cfg
+	return
 }
 
 func retry(d time.Duration, fn func()) {
@@ -113,7 +107,7 @@ func retry(d time.Duration, fn func()) {
 func main() {
 	ucfg, err := os.UserConfigDir()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("main", "os.UserConfigDir", err)
 	}
 
 	cfgpath := filepath.Join(ucfg, "onering.toml")
@@ -121,7 +115,6 @@ func main() {
 	go watch(cfgpath, &cfg)
 
 	http.HandleFunc("/", redirect)
-
 	go retry(time.Second*5, func() {
 		log.Println(http.ListenAndServe(cfg.Port, nil))
 	})
